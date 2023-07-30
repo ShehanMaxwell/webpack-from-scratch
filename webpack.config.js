04c987cd9ecad2ed3e382333e2e0771109a49207
Shehan Maxwell <shehanbbn@gmail.com>
2023-07-30 09:47:46 +0530

    modified 4 files

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-    module: {
-        rules:[
-            {
-                test: /\.(js|jsx)$/ ,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-             }
-        ]
-    }
-}
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
